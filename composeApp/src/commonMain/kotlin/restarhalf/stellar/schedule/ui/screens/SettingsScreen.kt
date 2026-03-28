780eb7b6002ee91d93139a6e0d53b59eba72d7b9
星痕 <15522353+restarhalf@user.noreply.gitee.com>
2026-03-28 14:08:10 +0800

    Setting页面顶栏

--- composeApp/src/commonMain/kotlin/restarhalf/stellar/schedule/ui/screens/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/restarhalf/stellar/schedule/ui/screens/SettingsScreen.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
@@ -20,6 +23,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
@@ -278,10 +282,19 @@ fun SettingsScreen(
         LazyColumn(
             modifier =
                 Modifier.fillMaxSize()
+                    .padding(
+                        PaddingValues(
+                            top = paddingValues.calculateTopPadding(),
+                            start =
+                                paddingValues.calculateStartPadding(LocalLayoutDirection.current),
+                            end = paddingValues.calculateEndPadding(LocalLayoutDirection.current),
+                            bottom = 0.dp
+                        )
+                    )
                     .pageScrollModifiers(scrollBehavior = topAppBarScrollBehavior),
             contentPadding =
                 appPageContentPadding(
-                    innerPadding = paddingValues,
+                    innerPadding = PaddingValues(),
                     outerPadding = appScaffoldPadding,
                     extraTop = 12.dp,
                     extraStart = 16.dp,
